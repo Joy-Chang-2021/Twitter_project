daa0c8ceac1c3cd4902332eebec152868b07684f
muchuan <mu.chuan.hung@gmail.com>
2022-02-24 22:52:00 +0800

    [新增] 後台管理建置
    vo.1 setup

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,17 @@ import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
+//避免非管理者進入管理者頁面
+// const authorizeIsAdmin = (to, from, next) => {
+//   const currentUser = store.state.currentUser
+//   if (currentUser && currentUser.role !== 'admin') {
+//     next('/404')
+//     return
+//   }
+//   next()
+// }
+
+
 const routes = [
   {
     path: '/',
@@ -20,6 +31,28 @@ const routes = [
     path: '/setting',
     name: 'setting',
     component: () => import('../views/Setting.vue')
+  },
+   {
+    path: '/admin',
+    name: 'admin-root',
+    redirect: '/admin/login'
+  },
+  {
+    path: '/admin/login',
+    name: 'admin-login',
+    component: () => import('../views/AdminLogin.vue')
+  },
+  {
+    path: '/admin/tweets',
+    name: 'admin-tweets',
+    component: () => import('../views/AdminTweets.vue'),
+    // beforeEnter: authorizeIsAdmin
+  },
+  {
+    path: '/admin/users',
+    name: 'admin-users',
+    component: () => import('../views/AdminUsers.vue'),
+    // beforeEnter: authorizeIsAdmin
   },
   {
     path: '*',
---
+++ src/views/AdminLogin.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="center-view">
+    <div class="admin-login">
+      <Toast :ToastMessage="ToastMessage" />
+      <div class="logo"></div>
+      <h2>後台登入</h2>
+      <account-login-form
+        @after-form-submit="afterFormSubmit"
+        :isProcessing="isProcessing"
+        :clearPasswordInput="clearPasswordInput"
+      />
+      <div class="login__actions">
+        <router-link class="link" to="/login">前台登入</router-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AccountLoginForm from "../components/AccountLoginForm.vue";
+import { Toast } from "../utils/helpers";
+// // import adminAPI from "../api/admin";
+
+export default {
+  components: {
+    AccountLoginForm,
+    Toast,
+  },
+  data() {
+    return {
+      // 母層須 props 此物件給 Toast 做為控制參數
+      ToastMessage: {
+        message: "",
+        dataStatus: "",
+      },
+      // 預設後端回傳錯誤訊息
+      backendReturnStatus: false,
+      // 防止使用者重複點擊
+      isProcessing: false,
+      clearPasswordInput: false,
+    };
+  },
+
+};
+</script>
